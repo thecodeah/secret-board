578f1ce6c86f2a113f8ff9211427b006dbb70f38
Codeah <codeah@protonmail.com>
2019-03-01 20:38:40 +0300

    Updated post form to a Django form

--- posts/views.py
+++ posts/views.py
@@ -3,8 +3,10 @@ import datetime
 from django.shortcuts import render, reverse
 from django.views import generic
 from django.http import HttpResponseRedirect, HttpResponse
+from django.utils import timezone
 
 from .models import Post
+from .forms import PostForm
 
 class FeedView(generic.ListView):
     model = Post
@@ -16,10 +18,11 @@ class FeedView(generic.ListView):
 
 def post(request):
     if request.method == "POST":
-        post = Post()
-        post.author = request.user
-        post.content = request.POST["content"]
-        post.pub_date = datetime.datetime.now()
-        post.save()
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit = False)
+            post.author = request.user
+            post.pub_date = timezone.now()
+            post.save()
 
     return HttpResponseRedirect(reverse("posts:feed"))
