b79d8d3a4b5f5b4300506ee41abd38113499fb81
Codeah <codeah@protonmail.com>
2019-03-07 15:23:49 +0300

    Added boards sidebar

--- posts/views.py
+++ posts/views.py
@@ -30,7 +30,7 @@ class FeedView(AjaxListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context.update({"board": self.board})
+        context.update({"board": self.board, "board_list": Board.objects.all() })
         return context
 
     def get_queryset(self):
