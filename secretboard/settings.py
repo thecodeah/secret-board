f69e0b38e17598fed28046085cd3fa8472dcfe20
Codeah <codeah@protonmail.com>
2019-03-11 15:51:17 +0300

    Changed standard sqlite3 db to MySQL database

--- secretboard/settings.py
+++ secretboard/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'secretboard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'secretboard',
+        'USER': 'secretboard',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
