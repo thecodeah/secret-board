9e80bc4ab0f97f376159c2af9fcbfa26960089c2
Codeah <codeah@protonmail.com>
2019-10-10 11:17:31 +0200

    Updated Docker setup - dev and prod compose files

--- secretboard/settings.py
+++ secretboard/settings.py
@@ -23,7 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv("SB_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+if os.getenv("SB_DEBUG"):
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
